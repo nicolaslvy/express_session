18b28124a7e81577a316c19eeebb9998f70aa044
asdfghj <nicolasmortizg@gmail.com>
2022-01-28 19:10:26 -0500

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require('express');
+// const cors = require('cors')
+const path = require('path')
 const { Server } = require('http');
 const app = express()
-const path = require('path')
 const PORT =  process.env.PORT || 3500;
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'views', 'index.htmlñ'));
+app.use((req,res,next)=>{
+    console.log(`${req.method}, 
+                  ${req.path}  `)
+    next()              
 })
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+})
+
+app.get('/new-page', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+})
+app.get('/old-page', (req, res)=>{
+    res.redirect("/new-page");
+})
+
+app.get('/*', (req, res)=>{
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
+
+
 
 app.listen(PORT,()=> console.log(`server runnig on port ${PORT}`))
