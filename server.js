494798c13dc2414302c2946193d584f65b0af3ff
asdfghj <nicolasmortizg@gmail.com>
2022-04-07 16:42:35 -0500

    stting router

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express');
-const cors = require('cors')
-const { logger } = require('./middleware/logEvents')
-const errorHandler = require('./middleware/errorHandler')
-const path = require('path')
-const { Server } = require('http');
-const app = express()
+const app = express();
+const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
 
-app.use(logger)
+// custom middleware logger
+app.use(logger);
 
-// || !origin
-
-const whitelist = ['https://www.google.com', 'http://127.0.0.1', 'http://localhost:3500/']
+// Cross Origin Resource Sharing
+const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
 const corsOptions = {
     origin: (origin, callback) => {
         if (whitelist.indexOf(origin) !== -1 || !origin) {
@@ -23,40 +22,35 @@ const corsOptions = {
     optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions));
-app.use(cors(corsOptions))
 
+// built-in middleware to handle urlencoded data
+// in other words, form data:  
+// ‘content-type: application/x-www-form-urlencoded’
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-})
+//serve static files
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
+// routes
 
-app.get('/new-page', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-})
-app.get('/old-page', (req, res) => {
-    res.redirect("/new-page");
-})
+app.use('/subdir', require('./routes/subdir'));
+
 
 app.all('*', (req, res) => {
     res.status(404);
     if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
     }
-    else if (req.accepts('json')) {
-        res.json({ error: "404 not found" })
-    }
-    else{
-        res.type('txt').send("404 not found")
-    }
+});
 
-})
+app.use(errorHandler);
 
-
-app.use(errorHandler)
-
-
-
-
-app.listen(PORT, () => console.log(`server runnig on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
